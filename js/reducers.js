78ab654857d95dd60b62b61c57f368dbf3b3e602
Eric Snell <eric_snell21@yahoo.com>
2016-08-03 16:39:40 -0400

    updated reducer file

--- js/reducers.js
+++ js/reducers.js
@@ -79,6 +79,53 @@ var gameReducer = function(state, action) {
 
 //     }
 //   )
+module.exports = gameReducer;
 
+
+
+
+/*==================== NEW REDUCER FILE ==========================*/
+
+var actions = require('./actions');
+
+var initialState = {
+	username: '',
+	score: 0,
+	overlay: false
+//timer
+//highScore
+};
+
+var gameReducer = function(state, action) {
+	state = state || initialState;
+
+	if (action.type === actions.NEW_GAME) {
+		var newInitialState = Object.assign({}, initialState);
+		return newInitialState;
+	}
+
+	else if (action.type === actions.INCREMENT_SCORE) {
+		var newScore = state.score++;
+		return Object.assign({}, state, {score: newScore})
+	}
+
+	else if (action.type === actions.DECREMENT_SCORE) {
+		var newScore = state.score--;
+		return Object.assign({}, state, {score: newScore})
+	}
+
+	else if (action.type === actions.TOGGLE_OVERLAY) {
+		if (state.overlay) {
+			return Object.assign({}, state, {overlay: false})
+		} else {
+			return Object.assign({}, state, {overlay: true})
+	}
+	// TODO: Add ADD_USER action to actions 
+	else if (action.type === actions.ADD_USER) {
+		userName = action.username;
+		return Object.assign({}, state, {username: userName})
+	}
+	return state;
+}
 
 module.exports = gameReducer;
