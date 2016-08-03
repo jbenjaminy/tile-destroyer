7af40065a34ae21afd0c98487955b93b7ed2eeb0
Eric Snell <eric_snell21@yahoo.com>
2016-08-03 18:20:22 -0400

    fixed syntax error in overlay-instructions

--- js/overlay-instructions.jsx
+++ js/overlay-instructions.jsx
@@ -4,7 +4,7 @@ var OverlayInstructions = React.createClass( {
 
   render: function() {
 
-      return (s
+      return (
         <div className="instructions">
           <h3>How to play</h3>
           <p>Press the PLAY button and the game will begin in three seconds. When the game starts, you will have 15 seconds to shoot all the red tiles. If you shoot blue tiles, you will be penalized. Press the PLAY button again to start another game.</p>
