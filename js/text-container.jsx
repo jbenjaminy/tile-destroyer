71e15f25a3075aa85532453e044e12b4e8cda92e
jbenjaminy <young.bejamin@gmail.com>
2016-08-03 10:08:44 -0500

    Removes comments in text-container

--- js/text-container.jsx
+++ js/text-container.jsx
@@ -1,19 +1,19 @@
-// var React = require('react');
+var React = require('react');
 
-// var Timer = require('./timer');
-// var Score = require('./score');
-// var BestScore = require('./best-score');
+var Timer = require('./timer');
+var Score = require('./score');
+var BestScore = require('./best-score');
 
-// var TextContainer = React.createClass({
-//   render: function() {
-//     return (
-//       <div>
-//         <p><BestScore /></p>
-//         <p><Score /></p>
-//         <p><Timer /></p>
-//       </div>
-//     );
-//   }
-// });
+var TextContainer = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p><BestScore /></p>
+        <p><Score /></p>
+        <p><Timer /></p>
+      </div>
+    );
+  }
+});
 
-// module.exports = TextContainer;
+module.exports = TextContainer;
