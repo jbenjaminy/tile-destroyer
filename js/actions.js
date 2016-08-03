7d67c025670287945594b25ef7bd5b3cef0ddeb3
jbenjaminy <young.bejamin@gmail.com>
2016-08-03 11:47:08 -0500

    Starts writing action creators

--- js/actions.js
+++ js/actions.js
@@ -1,10 +1,6 @@
-var TOGGLE_OVERLAY = 'TOGGLE_OVERLAY';
-var toggleOverlay = function() {
-  return {
-    type: TOGGLE_OVERLAY
-  };
-};
+/*---------- ACTIONS ---------*/
 
+// START GAME
 var NEW_GAME = 'NEW_GAME';
 var newGame = function() {
   return {
@@ -12,6 +8,15 @@ var newGame = function() {
   };
 };
 
+// VIEW INSTRUCTIONS
+var TOGGLE_OVERLAY = 'TOGGLE_OVERLAY';
+var toggleOverlay = function() {
+  return {
+    type: TOGGLE_OVERLAY
+  };
+};
+
+// CHANGE SCORE
 var INCREMENT_SCORE = 'INCREMENT_SCORE';
 var incrementScore = function() {
   return {
@@ -26,6 +31,28 @@ var decrementScore = function() {
   };
 };
 
+
+/*---------- FETCH ACTIONS ---------*/
+
+// POST NEW USER
+var fetchAddUser = function() {
+    return function(dispatch) {
+        var url = 'localhost:8080/games'
+        return fetch(url, {
+                    method: 'post',
+                    body: JSON.stringify({
+                            numGuesses: lastGame
+                        })
+    }).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            var lastGame = data
+            return dispatch(
+                fetchPostGameSuccess(lastGame)
+            );
+        });
+    }
+};
 
 var FETCH_ADD_USER_SUCCESS = 'FETCH_ADD_USER_SUCCESS';
 var fetchAddUserSuccess = function(userName) {
@@ -44,7 +71,27 @@ var fetchAddUserError = function(userName, error) {
     };
 };
 
-// Fetch  ADD_SCORE
+
+// POST NEW SCORE
+var fetchAddScore = function() {
+    return function(dispatch) {
+        var url = 'localhost:8080/games'
+        return fetch(url, {
+                    method: 'post',
+                    body: JSON.stringify({
+                            numGuesses: lastGame
+                        })
+    }).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            var lastGame = data
+            return dispatch(
+                fetchPostGameSuccess(lastGame)
+            );
+        });
+    }
+};
+
 var FETCH_ADD_SCORE_SUCCESS = 'FETCH_ADD_SCORE_SUCCESS';
 var fetchAddScoreSuccess = function(userID, score) {
     return {
@@ -63,13 +110,31 @@ var fetchAddScoreError = function(userID, error) {
     };
 };
 
-// Fetch GET_HISTORY
+
+// GET GAME HISTORY
+var fetchAddUser = function() {
+  console.log('here, bestgame');
+    return function(dispatch) {
+        var url = 'localhost:8080/games/best'
+        return fetch(url, {
+                    method: 'get'
+    }).then(function(response) {
+        console.log(response, 'get response');
+            return response.json();
+        }).then(function(data) {
+            var bestGame = data
+            return dispatch(
+                fetchBestGameSuccess(lastGame)
+            );
+        });
+    }
+};
+
 var FETCH_GET_HISTORY_SUCCESS = 'FETCH_GET_HISTORY_SUCCESS';
 var fetchGetHistorySuccess = function(userName) {
     return {
         type: FETCH_GET_HISTORY_SUCCESS,
         userName: userName
-
     };
 };
 
@@ -81,53 +146,72 @@ var fetchGetHistoryError = function(userName) {
     };
 };
 
-// Fetch GET_HIGHSCORE
-var FETCH_GET_HIGHSCORE_SUCCESS = 'FETCH_GET_HIGHSCORE_SUCCESS';
-var fetchGetHighScoreSuccess = function(userName) {
+
+// GET HIGH SCORE
+var fetchGetHighScore = function(username) {
+    return function(dispatch) {
+        var url = 'localhost:8080/games/' + username + '/highscore'
+        return fetch(url, {
+                    method: 'get'
+    }).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            var bestGame = data
+            return dispatch(
+                fetchBestGameSuccess(lastGame)
+            );
+        });
+    }
+};
+
+var FETCH_GET_HIGH_SCORE_SUCCESS = 'FETCH_GET_HIGH_SCORE_SUCCESS';
+var fetchGetHighScoreSuccess = function(username, highScore) {
     return {
         type: FETCH_GET_HIGHSCORE_SUCCESS,
-        userName: userName
-
+        username: username,
+        highScore: highScore
     };
 };
 
-var FETCH_GET_HIGHSCORE_ERROR = 'FETCH_GET_HIGHSCORE_ERROR';
-var fetchGetHighScoreError = function(userName) {
+var FETCH_GET_HIGH_SCORE_ERROR = 'FETCH_GET_HIGH_SCORE_ERROR';
+var fetchGetHighScoreError = function(username, error) {
     return {
         type: FETCH_GET_HIGHSCORE_ERROR,
-        userName: userName,
+        username: username,
+        error: error
     };
 };
 
 
-exports.TOGGLE_OVERLAY = TOGGLE_OVERLAY;
+/*---------- EXPORTS ---------*/
 exports.NEW_GAME = NEW_GAME;
-
-exports.toggleOverlay = toggleOverlay;
 exports.newGame = newGame;
 
+exports.TOGGLE_OVERLAY = TOGGLE_OVERLAY;
+exports.toggleOverlay = toggleOverlay;
+
+exports.INCREMENT_SCORE = INCREMENT_SCORE;
+exports.incrementScore = incrementScore;
+
+exports.DECREMENT_SCORE = DECREMENT_SCORE;
+exports.decrementScore = decrementScore;
 
 exports.FETCH_ADD_USER_SUCCESS = FETCH_ADD_USER_SUCCESS;
-exports.fetchAddUserSuccess = fetchAddUserSuccess;
 exports.FETCH_ADD_USER_ERROR = FETCH_ADD_USER_ERROR;
+exports.fetchAddUserSuccess = fetchAddUserSuccess;
 exports.fetchAddUserError = fetchAddUserError;
 
 exports.FETCH_ADD_SCORE_SUCCESS = FETCH_ADD_SCORE_SUCCESS;
-exports.fetchAddScoreSuccess = fetchAddScoreSuccess;
 exports.FETCH_ADD_SCORE_ERROR = FETCH_ADD_SCORE_ERROR;
+exports.fetchAddScoreSuccess = fetchAddScoreSuccess;
 exports.fetchAddScoreError = fetchAddScoreError;
 
 exports.FETCH_GET_HISTORY_SUCCESS = FETCH_GET_HISTORY_SUCCESS;
-exports.fetchGetHistorySuccess = fetchGetHistorySuccess;
 exports.FETCH_GET_HISTORY_ERROR = FETCH_GET_HISTORY_ERROR;
+exports.fetchGetHistorySuccess = fetchGetHistorySuccess;
 exports.fetchGetHistoryError  = fetchGetHistoryError ;
 
-exports.FETCH_GET_HIGHSCORE_SUCCESS = FETCH_GET_HIGHSCORE_SUCCESS;
+exports.FETCH_GET_HIGH_SCORE_SUCCESS = FETCH_GET_HIGH_SCORE_SUCCESS;
+exports.FETCH_GET_HIGH_SCORE_ERROR = FETCH_GET_HIGH_SCORE_ERROR;
 exports.fetchGetHighScoreSuccess = fetchGetHighScoreSuccess ;
-exports.FETCH_GET_HIGHSCORE_ERROR = FETCH_GET_HIGHSCORE_ERROR;
 exports.fetchGetHighScoreError = fetchGetHighScoreError;
-
-exports.INCREMENT_SCORE = INCREMENT_SCORE;
-exports.decrementScore = decrementScore;
-exports.DECREMENT_SCORE = DECREMENT_SCORE;
-exports.incrementScore = incrementScore;
