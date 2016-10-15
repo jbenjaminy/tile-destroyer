f416ed1953270f0b508f3a10b01ffa8dc4fda681
jbenjaminy <young.benjamin@gmail.com>
2016-10-14 23:00:46 -0500

    makes change to serve build from backend server

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var knex = require('knex')({
 var jsonParser = bodyParser.json();
 var app = express();
 
+app.use(express.static('./build'));
+
 app.use(function(request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
